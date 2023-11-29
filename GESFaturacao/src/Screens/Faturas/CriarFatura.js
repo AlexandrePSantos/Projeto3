ea188f2e17eda5bfe338bb9013069318f51a106f
gomesam <gomesam2000@gmail.com>
2023-11-29 16:39:21 +0000

    Update CriarFatura.js

--- GESFaturacao/src/Screens/Faturas/CriarFatura.js
+++ GESFaturacao/src/Screens/Faturas/CriarFatura.js
@@ -1,29 +1,43 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Button, StyleSheet, Text, View, TouchableOpacity, ScrollView,ToastAndroid, } from 'react-native';
-import { AuthContext } from "../../Context/AuthContext";
-import { Picker } from '@react-native-picker/picker';
+import React, {useState, useEffect, useContext} from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  ToastAndroid,
+} from 'react-native';
+import {AuthContext} from '../../Context/AuthContext';
+import {Picker} from '@react-native-picker/picker';
 import DatePicker from 'react-native-date-picker';
 import moment from 'moment/moment';
 
-export default function CriarFatura({ navigation }) {
-  const { CriarFatura } = useContext(AuthContext);
-  const { getClientes } = useContext(AuthContext);
-  const { getSeries } = useContext(AuthContext);
+export default function CriarFatura({navigation}) {
+  const {CriarFatura} = useContext(AuthContext);
+  const {getClientes} = useContext(AuthContext);
+  const {getSeries} = useContext(AuthContext);
 
   const [dadosClientes, setDadosClientes] = useState([]);
   const [dadosSeries, setDadosSeries] = useState([]);
   const [datei, setDatei] = useState();
+  const [referenciaC, setReferencia] = useState('');
+  const [moedaC, setMoeda] = useState('1'); // Valor inicial '1' para 'Euro (€)'
+  const [descontoC, setDesconto] = useState('0'); // Valor inicial '0'
+  const [observacoesC, setObservacao] = useState('');
+  const [finalizarDocumentoC, setFinalizarDocumento] = useState(0);
   const [open, setOpen] = useState(false);
   const [clienteC, setCliente] = useState();
   const [serieC, setSerie] = useState();
-  const [dataC, setData] = useState("01/12/2023");
-  
+  const [dataC, setData] = useState('01/12/2023');
+
   const [datev, setDatev] = useState();
   const [openv, setOpenV] = useState(false);
   const [selectedIdCliente, setSelectedIdCliente] = useState(null);
   const [selectedIdSerie, setSelectedIdSerie] = useState(null);
-  
-  
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -62,87 +76,204 @@ export default function CriarFatura({ navigation }) {
     const LinhasC = '';
     const finalizarDocumentoC = '';
 
-    console.log(clienteC)
+    console.log(clienteC);
     CriarFatura(
-      clienteC, serieC, 
-      numeroC, dataC, 
-      validadeC, referenciaC, 
-      vencimentoC, moedaC, 
-      descontoC, observacoesC, 
-      LinhasC, finalizarDocumentoC
+      clienteC,
+      serieC,
+      numeroC,
+      dataC,
+      validadeC,
+      referenciaC,
+      vencimentoC,
+      moedaC,
+      descontoC,
+      observacoesC,
+      LinhasC,
+      finalizarDocumentoC,
     ).then(response => {
-      console.log(response + ' Resposta Criar Orçamento')
+      console.log(response + ' Resposta Criar Orçamento');
       navigation.navigate('GesFaturação');
-      ToastAndroid.show("Fatura Criada ", ToastAndroid.SHORT);
+      ToastAndroid.show('Fatura Criada ', ToastAndroid.SHORT);
     });
-  }
+  };
 
   return (
     <ScrollView>
-      <View style={styles.container}>     
+      <View style={styles.container}>
         <View style={{marginTop: 10}}>
           {/* Cliente */}
           <Text style={styles.titleSelect}>Client</Text>
           <View style={styles.borderMargin}>
-            <Picker style={styles.pickerComponent} selectedValue={selectedIdCliente} onValueChange={itemValue => { setSelectedIdCliente(itemValue); setCliente(itemValue); }} >
+            <Picker
+              style={styles.pickerComponent}
+              selectedValue={selectedIdCliente}
+              onValueChange={itemValue => {
+                setSelectedIdCliente(itemValue);
+                setCliente(itemValue);
+              }}>
               <Picker.Item label="Selecione um cliente" value={null} />
-              {dadosClientes.map(function (client, i) { return <Picker.Item label={client.name} value={client.id.toString()} key={i} />; })}
+              {dadosClientes.map(function (client, i) {
+                return (
+                  <Picker.Item
+                    label={client.name}
+                    value={client.id.toString()}
+                    key={i}
+                  />
+                );
+              })}
             </Picker>
           </View>
           {/* Serie */}
           <Text style={styles.titleSelect}>Series</Text>
-            <View style={styles.borderMargin}>
-              <Picker style={styles.pickerComponent} selectedValue={selectedIdSerie} onValueChange={itemValue => {setSelectedIdSerie(itemValue); setSerie(itemValue)}} >
-                <Picker.Item label="Selecione uma serie" value={null} />
-                {dadosSeries.map(function (serie, i) { return <Picker.Item label={serie.description} value={serie.id.toString()} key={i} />; })}
-              </Picker>
-            </View>
+          <View style={styles.borderMargin}>
+            <Picker
+              style={styles.pickerComponent}
+              selectedValue={selectedIdSerie}
+              onValueChange={itemValue => {
+                setSelectedIdSerie(itemValue);
+                setSerie(itemValue);
+              }}>
+              <Picker.Item label="Selecione uma serie" value={null} />
+              {dadosSeries.map(function (serie, i) {
+                return (
+                  <Picker.Item
+                    label={serie.description}
+                    value={serie.id.toString()}
+                    key={i}
+                  />
+                );
+              })}
+            </Picker>
+          </View>
           {/* date */}
           <Text style={styles.titleSelect}>Data</Text>
-            <View style={styles.borderMargin}>
-              <TouchableOpacity  onPress={() => setOpen(true)} style={styles.touchableO}>
-                <DatePicker modal mode="date" open={openv} date={new Date()}
-                  onConfirm={(datev) => { setOpenV(false); setDatev(datev); setValidade(moment(datev).format("DD/MM/YYYY")) }} onCancel={() => { setOpenV(false) }} />
-                <Text style={styles.textDate}> {todaiDate = moment(datei).format("DD/MM/YYYY") }</Text>
-              </TouchableOpacity>
-            </View>
+          <View style={styles.borderMargin}>
+            <TouchableOpacity
+              onPress={() => setOpen(true)}
+              style={styles.touchableO}>
+              <DatePicker
+                modal
+                mode="date"
+                open={openv}
+                date={new Date()}
+                onConfirm={datev => {
+                  setOpenV(false);
+                  setDatev(datev);
+                  setValidade(moment(datev).format('DD/MM/YYYY'));
+                }}
+                onCancel={() => {
+                  setOpenV(false);
+                }}
+              />
+              <Text style={styles.textDate}>
+                {' '}
+                {(todaiDate = moment(datei).format('DD/MM/YYYY'))}
+              </Text>
+            </TouchableOpacity>
+          </View>
           {/* expiration */}
           <Text style={styles.titleSelect}>Validade</Text>
-            <View style={styles.borderMargin}>
-              <TouchableOpacity  onPress={() => setOpenV(true)} style={styles.touchableO}>
-                <DatePicker modal mode="date" open={openv} date={new Date()}
-                  onConfirm={(datev) => { setOpenV(false); setDatev(datev); setValidade(moment(datev).format("DD/MM/YYYY")) }} onCancel={() => { setOpenV(false) }} />
-                <Text style={styles.textDate}> {todayVDate = moment(datev).format("DD/MM/YYYY") }</Text>
-              </TouchableOpacity>
-            </View>
+          <View style={styles.borderMargin}>
+            <TouchableOpacity
+              onPress={() => setOpenV(true)}
+              style={styles.touchableO}>
+              <DatePicker
+                modal
+                mode="date"
+                open={openv}
+                date={new Date()}
+                onConfirm={datev => {
+                  setOpenV(false);
+                  setDatev(datev);
+                  setValidade(moment(datev).format('DD/MM/YYYY'));
+                }}
+                onCancel={() => {
+                  setOpenV(false);
+                }}
+              />
+              <Text style={styles.textDate}>
+                {' '}
+                {(todayVDate = moment(datev).format('DD/MM/YYYY'))}
+              </Text>
+            </TouchableOpacity>
+          </View>
           {/* reference */}
-
+          <Text style={styles.titleSelect}>Referencia</Text>
+          <View style={styles.borderMargin}>
+            <TextInput
+              style={styles.input}
+              value={referenciaC}
+              onChangeText={text => setReferencia(text)}
+              placeholder="Referencia"
+            />
+          </View>
           {/* dueDate */}
-
+          {/* A espera do Engenheiro */}
           {/* Coin */}
-
+          <Text style={styles.titleSelect}>Moeda</Text>
+          <View style={styles.borderMargin}>
+            <Picker
+              selectedValue={moedaC}
+              onValueChange={itemValue => setMoeda(itemValue)}
+              style={styles.pickerComponent}>
+              <Picker.Item label="Euro (€)" value="1" />
+              <Picker.Item label="Libra ING (GBP)" value="2" />
+              <Picker.Item label="Dólar USA ($)" value="3" />
+              <Picker.Item label="Real Br. (R$)" value="4" />
+              <Picker.Item label="Fr. Suiço (CHF)" value="5" />
+            </Picker>
+          </View>
           {/* discount */}
-
+          <Text style={styles.titleSelect}>Desconto</Text>
+          <View style={styles.borderMargin}>
+            <TextInput
+              style={styles.input}
+              value={descontoC}
+              onChangeText={text => setDesconto(text)}
+              placeholder="Desconto"
+              keyboardType="numeric"
+            />
+          </View>
           {/* observations */}
-
+          <Text style={styles.titleSelect}>Observações</Text>
+          <View style={styles.borderMargin}>
+            <TextInput
+              style={styles.input}
+              value={observacoesC}
+              onChangeText={text => setObservacao(text)}
+              placeholder="Observações"
+            />
+          </View>
           {/* finalize */}
+          <Text style={styles.titleSelect}>Finalize</Text>
+          <View style={styles.borderMargin}>
+            <Picker
+              style={styles.pickerComponent}
+              placeholder="Finalizado"
+              selectedValue={finalizarDocumentoC}
+              onValueChange={itemValue => setFinalizarDocumento(itemValue)}>
+              <Picker.Item label="Rascunho" value="0" />
+              <Picker.Item label="Aberto" value="1" />
+            </Picker>
+          </View>
 
           {/* payment */}
 
           {/* lines/artigos */}
 
           {/* doc_origin */}
-
         </View>
-        <View style={{marginTop: 30,marginBottom: 10 ,width: 350}}>
-          <Button  title="Criar Fatura" color="#d0933f" onPress={() => handleCreateFatura()} />
+        <View style={{marginTop: 30, marginBottom: 10, width: 350}}>
+          <Button
+            title="Criar Fatura"
+            color="#d0933f"
+            onPress={() => handleCreateFatura()}
+          />
         </View>
       </View>
     </ScrollView>
   );
-
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -150,7 +281,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#e5e9ec',
     alignItems: 'center',
     justifyContent: 'flex-start',
-
   },
   button: {
     alignItems: 'center',
@@ -160,29 +290,26 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   textfont: {
-    color: "#ffffff",
+    color: '#ffffff',
     fontSize: 16,
-    fontweight: "bold"
-
+    fontweight: 'bold',
   },
   titleSelect: {
     fontSize: 20,
     margin: 10,
-    fontWeight: "bold",
-    color: "#5F5D5C"
+    fontWeight: 'bold',
+    color: '#5F5D5C',
   },
   pickerComponent: {
     width: 350,
-
   },
   borderMargin: {
     borderWidth: 1,
     borderColor: 'grey',
-
   },
   touchableO: {
     width: 350,
     height: 55,
-    justifyContent: "center"
-  }
+    justifyContent: 'center',
+  },
 });
