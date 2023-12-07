628ef7c9ac3e0681c77ad5b58707213b7a15cd54
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2023-12-07 15:36:39 +0000

    datas

--- GESFaturacao/src/Context/AuthContext.js
+++ GESFaturacao/src/Context/AuthContext.js
@@ -22,7 +22,7 @@ export const AuthProvider = ({children}) => {
         setIsLoading(true);
         try {
             const res = await axios.post(`${BASE_URL}/authentication`, { username, password });
-            console.log(res.data);
+            // console.log(res.data);
             let userInfo = res.data;
             setUserInfo(userInfo);
             setUserToken(userInfo._token);
@@ -30,7 +30,7 @@ export const AuthProvider = ({children}) => {
             await AsyncStorage.setItem('@userInfo', JSON.stringify(userInfo));
             await AsyncStorage.setItem('@userToken', userInfo._token);
     
-            console.log("User Token: " + userInfo._token);
+            // console.log("User Token: " + userInfo._token);
             ToastAndroid.show("Bem-vindo, " + username, ToastAndroid.SHORT);
     
             // Validate token
@@ -39,7 +39,7 @@ export const AuthProvider = ({children}) => {
                     'Authorization': userInfo._token,
                 }
             });
-            console.log(tokenRes.data);
+            // console.log(tokenRes.data);
     
             // Prepare the data for the validate-version request
             let data = qs.stringify({
@@ -61,7 +61,7 @@ export const AuthProvider = ({children}) => {
     
             // Validate version
             const versionRes = await axios.request(config);
-            console.log(versionRes.data);
+            // console.log(versionRes.data);
         } catch(e) {
             console.log(`Login error ${e}`);
         } finally {
@@ -77,38 +77,63 @@ export const AuthProvider = ({children}) => {
         setIsLoading(false);
     }
 
+    // ------!-------   
+    //  M. Pagamento
+    // ------!-------
+    const getMetodos = async () => {
+        var token = await this.getToken();
+        let data = qs.stringify({ });
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: 'https://devipvc.gesfaturacao.pt/gesfaturacao/server/webservices/api/mobile/v1.0.2/payment-methods',
+            headers: { 
+              'Authorization': token
+            },
+            data : data
+          };
+    
+        return axios.request(config)
+        .then((response) => {
+        // console.log(JSON.stringify(response.data));
+        return response.data; 
+        })
+        .catch((error) => {
+        console.log(error);
+        });
+    }
+
     // ------!-------
     //    Faturas
     // ------!-------
-    const CriarFatura = async (clienteC, serieC, dataC, validadeC, referenciaC, vencimentoC, moedaC, descontoC, observacoesC, LinhasC, finalizarDocumentoC) => {
+    const CriarFatura = async (clienteC, serieC, numeroC, dataC, validadeC, dueDateC, referenciaC, moedaC, descontoC, observacoesC, metodoC, finalizarDocumentoC) => {
         var token = await this.getToken();
-        const stringifiedLinhas = JSON.stringify(LinhasC);
+        // const stringifiedLinhas = JSON.stringify(LinhasC);
     
         let data = qs.stringify({
             'client': clienteC,
             'serie': serieC,
-            'number': 0,
+            'number': numeroC,
             'date': dataC,
             'expiration': validadeC,
             'reference': referenciaC,
-            'dueDate': vencimentoC,
+            'dueDate': 0,
             'coin': moedaC,
             'discount': descontoC,
             'observations': observacoesC,
             'finalize': finalizarDocumentoC,
-            'payment': 0,
-            'lines': stringifiedLinhas,
+            'payment': metodoC,
+            'lines': '[{"id":"113","description":"Tinteiro Compatível Epson T1631 Preto","quantity":"1","price":"1.22","discount":"0","tax":1,"exemption":"0","retention":0}]',
             'doc_origin': '9' 
         });
     
         let config = {
             method: 'post',
             maxBodyLength: Infinity,
-            url: `${BASE_URL}/invoices`,
+            url: `https://devipvc.gesfaturacao.pt/gesfaturacao/server/webservices/api/mobile/v1.0.2/invoices`,
             headers: { 
                 'Content-Type': 'application/x-www-form-urlencoded', 
                 'Authorization': token, 
-                'Cookie': 'PHPSESSID=2126001ea125fd6cd0c8d1029eb1497a'
             },
             data : data
         };
@@ -141,7 +166,7 @@ export const AuthProvider = ({children}) => {
         return axios.request(config)
         .then((response) => {
         // console.log(JSON.stringify(response.data));
-        return response.data; // Add this line
+        return response.data; 
         })
         .catch((error) => {
         console.log(error);
@@ -165,7 +190,7 @@ export const AuthProvider = ({children}) => {
         return axios.request(config)
         .then((response) => {
         // console.log(JSON.stringify(response.data));
-        return response.data; // Add this line
+        return response.data; 
         })
         .catch((error) => {
         console.log(error + ' Erro Series');
@@ -189,7 +214,7 @@ export const AuthProvider = ({children}) => {
         return axios.request(config)
         .then((response) => {
         // console.log(JSON.stringify(response.data));
-        return response.data; // Add this line
+        return response.data; 
         })
         .catch((error) => {
         console.log(error);
@@ -272,6 +297,7 @@ export const AuthProvider = ({children}) => {
             getArtigos, 
             CriarFatura,
             getClientes, 
+            getMetodos,
             isLoading, userToken}}>
             {children}
         </AuthContext.Provider>
--- GESFaturacao/src/Screens/Faturas/CriarFatura.js
+++ GESFaturacao/src/Screens/Faturas/CriarFatura.js
@@ -17,40 +17,52 @@ import DatePicker from 'react-native-date-picker';
 import moment from 'moment/moment';
 
 export default function CriarFatura({ navigation }) {
+  // VARIAVEIS PARA OBTER OS DADOS DOS CLIENTES, SERIES, ARTIGOS E METODOS
+  // SÃO USADOS PARA CARREGAR ARRAYS DOS PICKERS
   const { CriarFatura } = useContext(AuthContext);
   const { getClientes } = useContext(AuthContext);
   const { getSeries } = useContext(AuthContext);
   const { getArtigos } = useContext(AuthContext);
+  const { getMetodos } = useContext(AuthContext);
 
+  // ARRAYS PARA GUARDAR OS DADOS DOS CLIENTES, SERIES, ARTIGOS E METODOS
+  // SÃO MOSTRADOS NOS PICKERS
   const [dadosClientes, setDadosClientes] = useState([]);
   const [dadosSeries, setDadosSeries] = useState([]);
+  const [dadosMetodo, setDadosMetodo] = useState([]);
   const [dadosArtigos, setDadosArtigos] = useState([]);
 
-  
-  const [referenciaC, setReferencia] = useState('');
-  const [moedaC, setMoeda] = useState('1'); // Valor inicial '1' para 'Euro (€)'
-  const [descontoC, setDesconto] = useState('0'); // Valor inicial '0'
-  const [observacoesC, setObservacao] = useState('');
-  const [finalizarDocumentoC, setFinalizarDocumento] = useState(0);
-  const [clienteC, setCliente] = useState();
-  const [serieC, setSerie] = useState();
-
-  const [dataC, setData] = useState();
-  const [openc, setopenc] = useState(false);
-  const [dataV, setDatev] = useState(new Date());
-  const [openv, setopenv] = useState(false);
-  const [validadeC, setValidade] = useState('');
-
+  // VARIAVEIS PARA GUARDAR OS IDS DOS CLIENTES, SERIES, ARTIGOS E METODOS SELECIONADOS NOS PICKERS
   const [selectedIdCliente, setSelectedIdCliente] = useState(null);
   const [selectedIdSerie, setSelectedIdSerie] = useState(null);
   const [selectedIdArtigo, setSelectedIdArtigo] = useState(null);
+  const [selectedMetodo, setSelectedIdMetodo] = useState(null);
 
+  // VARIAVEIS PARA GUARDAR OS DADOS DA FATURA
+  // SÃO USADOS PARA ENVIAR PARA A API
+  const [ref, setReferencia] = useState('');
+  const [moeda, setMoeda] = useState('1'); // Valor inicial '1' para 'Euro (€)'
+  const [desc, setDesconto] = useState('0'); // Valor inicial '0'
+  const [obs, setObservacao] = useState('');
+  const [finalizarDoc, setFinalizarDocumento] = useState(0);
+  const [cliente, setCliente] = useState();
+  const [serie, setSerie] = useState();
+  const [dataIni, setDataIni] = useState(moment().format('DD/MM/YYYY'));
+  const [dataVal, setDataVal] = useState(moment().format('DD/MM/YYYY'));
+  const [vencimento, setVencimento] = useState('');
+  const [metodo, setMetodo] = useState('');
+
+  const [openc, setopenc] = useState(false);
+  const [openv, setopenv] = useState(false);
+
+  // METODO PARA OBTER OS DADOS DOS CLIENTES, SERIES, ARTIGOS E METODOS
   useEffect(() => {
     const fetchData = async () => {
       try {
         const clientesResponse = await getClientes();
         const seriesResponse = await getSeries();
         const artigosResponse = await getArtigos();
+        const metodosResponse = await getMetodos();
 
         if (clientesResponse.data) {
           setDadosClientes(clientesResponse.data);
@@ -63,47 +75,52 @@ export default function CriarFatura({ navigation }) {
         if (artigosResponse.data) {
           setDadosArtigos(artigosResponse.data);
         }
-
+        if (metodosResponse.data) {
+          setDadosMetodo(metodosResponse.data);
+        }
       } catch (error) {
         console.error(error);
       }
     };
-
     fetchData();
   }, []);
 
+  // METODO EXECUTADO QUANDO O BOTÃO DE CRIAR FATURA É PRESSIONADO
+  // ENVIA OS DADOS PARA A API
   const handleCreateFatura = () => {
     // Define the variables here
-    const clienteC = clienteC;
-    const serieC = serieC;
-    const numeroC = '';
-    const dataC = '';
-    const validadeC = '';
-    const referenciaC = '';
-    const vencimentoC = '';
-    const moedaC = '';
-    const descontoC = '';
-    const observacoesC = '';
-    const LinhasC = '';
-    const finalizarDocumentoC = '';
+    const clienteC = cliente;
+    const serieC = serie;
+    const numeroC = 0;
+    const dataC = dataIni; // está undifined
+    const validadeC = dataVal; // está undifined
+    const dueDateC = vencimento;
+    const referenciaC = ref; 
+    const moedaC = moeda;
+    const descontoC = desc;
+    const observacoesC = obs;
+    // const LinhasC = linha;
+    const metodoC = metodo;
+    const finalizarDocumentoC = finalizarDoc;
   
-    console.log(clienteC);
     CriarFatura(
       clienteC,
       serieC,
       numeroC,
       dataC,
       validadeC,
+      dueDateC,
       referenciaC,
-      vencimentoC,
       moedaC,
       descontoC,
       observacoesC,
-      LinhasC,
+    //   // LinhasC,
+      metodoC,
       finalizarDocumentoC,
     ).then(response => {
+      console.log(clienteC, serieC, numeroC, dataC, validadeC, dueDateC, referenciaC, moedaC, descontoC, observacoesC, metodoC, finalizarDocumentoC);
       console.log(response + ' Resposta Criar Fatura');
-      navigation.navigate('GesFaturação');
+    //   // navigation.navigate('GesFaturação');
       ToastAndroid.show('Fatura Criada ', ToastAndroid.SHORT);
     });
   };
@@ -112,6 +129,7 @@ export default function CriarFatura({ navigation }) {
     <ScrollView>
       <View style={styles.container}>
         <View style={{marginTop: 10}}>
+
           {/* Cliente */}
           <Text style={styles.titleSelect}>Client</Text>
           <View style={styles.borderMargin}>
@@ -133,6 +151,7 @@ export default function CriarFatura({ navigation }) {
               ))}
             </Picker>
           </View>
+
           {/* Serie */}
           <Text style={styles.titleSelect}>Series</Text>
           <View style={styles.borderMargin}>
@@ -154,67 +173,73 @@ export default function CriarFatura({ navigation }) {
               ))}
             </Picker>
           </View>
-          {/* date */}
+
+          {/* date - DONE */}
           <Text style={styles.titleSelect}>Data</Text>
           <View style={styles.borderMargin}>
             <TouchableOpacity onPress={() => setopenc(true)} style={styles.touchableO}>
-              <DatePicker 
-                modal 
-                mode="date" 
-                open={openc} 
-                date={new Date()} 
-                onConfirm={dataC => {
-                  setopenc(false);
-                  if (moment(dataC).isBefore(moment(validadeC, 'DD/MM/YYYY'))) {
-                    setData(dataC);
-                  } else {
-                    alert('Selected date must be less than validade');
-                  }
-                }} 
-                onCancel={() => setopenc(false)}
-              />
-              <Text> {' '} {(todaiDate = moment(dataC).format('DD/MM/YYYY'))}</Text>
-            </TouchableOpacity>
-          </View>
-          {/* expiration */}
-          <Text style={styles.titleSelect}>Validade</Text>
-          <View style={styles.borderMargin}>
-            <TouchableOpacity onPress={() => setopenv(true)} style={styles.touchableO}>
-              <DatePicker 
-                modal 
-                mode="date" 
-                open={openv} 
-                date={dataV} 
-                onConfirm={validadeC => { 
-                  setopenv(false);
-                  setDatev(validadeC);
-                  setValidade(moment(validadeC).format('DD/MM/YYYY'));
-                }} 
-                onCancel={() => { setopenv(false); }} 
-              />
-              <Text style={styles.textDate}> {' '} {moment(dataV).format('DD/MM/YYYY')}</Text>
+            <DatePicker
+              modal 
+              mode="date"
+              open={openc}
+              date={new Date(moment(dataIni, 'DD/MM/YYYY').format())}
+              minimumDate={new Date()}
+              onConfirm={date => {
+                setopenc(false);
+                if (moment(date).isBefore(moment(dataVal, 'DD/MM/YYYY'))) {
+                  setDataIni(moment(date).format('DD/MM/YYYY'));
+                } else {
+                  ToastAndroid.show('Data de inicio não pode ser após a data de validade', ToastAndroid.SHORT);
+                }
+              }}
+              onCancel={() => setopenc(false)}
+            />
+            <Text style={styles.textDate}> {' '} {dataIni}</Text>
             </TouchableOpacity>
           </View>
 
-          {/* reference */}
+          {/* expiration - DONE */}
+          <Text style={styles.titleSelect}>Validade</Text>
+          <View style={styles.borderMargin}>
+            <TouchableOpacity onPress={() => setopenv(true)} style={styles.touchableO}>
+            <DatePicker
+              modal 
+              mode="date"
+              open={openv}
+              date={new Date(moment(dataVal, 'DD/MM/YYYY').format())}
+              minimumDate={new Date()}
+              onConfirm={date => {
+                setopenv(false);
+                if (moment(date).isAfter(moment(dataIni, 'DD/MM/YYYY'))) {
+                  setDataVal(moment(date).format('DD/MM/YYYY'));
+                  const vencimentoEmDias = moment(date).diff(moment(dataIni, 'DD/MM/YYYY'), 'days');
+                  setVencimento(vencimentoEmDias);
+                } else {
+                  ToastAndroid.show('Data de validade não pode ser anterior à data de inicio', ToastAndroid.SHORT);
+                }
+              }}
+              onCancel={() => setopenv(false)}
+            />
+              <Text style={styles.textDate}> {' '} {dataVal}</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* reference - DONE */}
           <Text style={styles.titleSelect}>Referencia</Text>
           <View style={styles.borderMargin}>
             <TextInput
               style={styles.input}
-              value={referenciaC}
+              value={ref}
               onChangeText={text => setReferencia(text)}
               placeholder="Referencia"
             />
           </View>
 
-          {/* dueDate */}
-          {/* A espera do Engenheiro */}
-
-          {/* Coin */}
+          {/* Coin - DONE */}
           <Text style={styles.titleSelect}>Moeda</Text>
           <View style={styles.borderMargin}>
             <Picker
-              selectedValue={moedaC}
+              selectedValue={moeda}
               onValueChange={itemValue => setMoeda(itemValue)}
               style={styles.pickerComponent}
             >
@@ -226,36 +251,36 @@ export default function CriarFatura({ navigation }) {
             </Picker>
           </View>
 
-          {/* discount */}
+          {/* discount - DONE */}
           <Text style={styles.titleSelect}>Desconto</Text>
           <View style={styles.borderMargin}>
             <TextInput
               style={styles.input}
-              value={descontoC}
+              value={desc}
               onChangeText={text => setDesconto(text)}
               placeholder="Desconto"
               keyboardType="numeric"
             />
           </View>
 
-          {/* observations */}
+          {/* observations - DONE */}
           <Text style={styles.titleSelect}>Observações</Text>
           <View style={styles.borderMargin}>
             <TextInput
               style={styles.input}
-              value={observacoesC}
+              value={obs}
               onChangeText={text => setObservacao(text)}
               placeholder="Observações"
             />
           </View>
 
-          {/* finalize */}
+          {/* finalize - DONE */}
           <Text style={styles.titleSelect}>Finalize</Text>
           <View style={styles.borderMargin}>
             <Picker
               style={styles.pickerComponent}
               placeholder="Finalizado"
-              selectedValue={finalizarDocumentoC}
+              selectedValue={finalizarDoc}
               onValueChange={itemValue => setFinalizarDocumento(itemValue)}
             >
               <Picker.Item label="Rascunho" value="0" />
@@ -264,10 +289,36 @@ export default function CriarFatura({ navigation }) {
           </View>
 
           {/* payment */}
+          <Text style={styles.titleSelect}>Método de Pagamento</Text>
+          <View style={styles.borderMargin}>
+            <Picker
+              style={styles.pickerComponent}
+              placeholder="Método de Pagamento"
+              selectedValue={metodo}
+              onValueChange={itemValue => {
+              setSelectedIdMetodo(itemValue); 
+              setMetodo(itemValue);}}
+            >
+              <Picker.Item label="Selecione um método de pagamento" value={null} />
+              {dadosMetodo.map((metodo, i) => (
+                <Picker.Item
+                  label={metodo.name}
+                  value={metodo.id.toString()}
+                  key={i}
+                />
+              ))}
+            </Picker>
+          </View>
 
           {/* lines/artigos */}
+          {/* Deve permitir selecionar vários artigos e as quantidades de cada */}
+          <Text style={styles.titleSelect}>Artigos</Text>
+          <View style={styles.borderMargin}>
+              
+          </View>
 
           {/* doc_origin */}
+
           </View>
           <View style={{marginTop: 30, marginBottom: 10, width: 350}}>
             <Button
