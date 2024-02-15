005cdf3deeeabdd839fe7b7061adc68c230447bd
gomesam <gomesam2000@gmail.com>
2024-02-15 14:25:18 +0000

    Listar

--- GESFaturacao/src/Screens/Artigos/ListarArtigos.js
+++ GESFaturacao/src/Screens/Artigos/ListarArtigos.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Button, Image, StyleSheet, Text, View, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import LinearGradient from 'react-native-linear-gradient';
 
 import { AuthContext } from '../../Context/AuthContext';
 
@@ -7,11 +9,12 @@ export default function ListarArtigos({ navigation }) {
   const { getArtigos, getArtigoID, removerArtigo } = useContext(AuthContext);
   const [artigos, setArtigos] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [filtro, setFiltro] = useState('Todos');
 
   useEffect(() => {
     carregarArtigos();
   }, []);
-  
+
   const carregarArtigos = async () => {
     try {
       const response = await getArtigos();
@@ -30,7 +33,7 @@ export default function ListarArtigos({ navigation }) {
 
   const handleRemoverArtigo = async (artigoId) => {
     try {
-      const response = await removerArtigo(artigoId);
+      await removerArtigo(artigoId);
       carregarArtigos();
     } catch (error) {
       console.error('Erro ao remover Artigo:', error);
@@ -39,42 +42,62 @@ export default function ListarArtigos({ navigation }) {
 
   const keyExtractor = (item) => item.id.toString();
 
-  if (loading) {
+  const CustomButtonModal = ({ title, onPress, styles, gradientColors }) => (
+    <TouchableOpacity onPress={onPress}>
+      <LinearGradient
+        colors={gradientColors}
+        style={[styles.buttonModal]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+      >
+        <Text style={styles.modalText}>{title}</Text>
+      </LinearGradient>
+    </TouchableOpacity>
+  );
+  const renderItem = ({ item: artigo }) => {
+    if ((filtro === 'Usados' && artigo.locked !== 1) || (filtro === 'Não Usados' && artigo.locked === 1)) {
+      return null;
+    }
+  
     return (
-      <View style={styles.loading}>
-        <ActivityIndicator size="large" color='#d0933f' />
+      <View style={styles.artigoContainer}>
+        <TouchableOpacity onPress={() => navigation.navigate('Detalhes Artigo', { artigoId: artigo.id })}>
+          <View style={styles.rowContainer}>
+            <View style={styles.artigoInfo}>
+              <Text style={[styles.text, { marginTop: 5 }]}>ID: {artigo.id.toString()}</Text>
+              <Text style={styles.text}>Preço: {parseFloat(artigo.price).toFixed(2)}</Text>
+              <Text style={styles.text}>Preço+Tax: {parseFloat(artigo.pricePvp).toFixed(2)}€</Text>
+              <Text style={styles.text}>Descrição: {artigo.description.toString()}</Text>
+            </View>
+            <Image source={{ uri: artigo.image.toString() }} style={styles.artigoImage} />
+          </View>
+        </TouchableOpacity>
+        {(filtro === 'Todos' || (filtro === 'Não Usados' && artigo.locked !== 1)) && artigo.locked !== 1 && (
+
+          <View style={styles.buttonModal}>
+          <CustomButtonModal
+            styles={styles}
+            gradientColors={['#ff0000', '#ffa500']}
+            title="Remover" 
+            onPress={() => handleRemoverArtigo(artigo.id)}/>
+          </View>
+        )}
       </View>
     );
-  }
+  };
   
-  const renderItem = ({ item: artigo }) => (
-    <View style={styles.artigoContainer}>
-      <TouchableOpacity onPress={() => navigation.navigate('Detalhes Artigo', { artigoId: artigo.id })} >
-        <View style={styles.rowContainer}>
-          <View style={styles.artigoInfo}>
-            <Text style={[styles.text, { marginTop: 5 }]}>ID: {artigo.id.toString()}</Text>
-            <Text style={styles.text}>Preço: {parseFloat(artigo.price).toFixed(2)}</Text>
-            <Text style={styles.text}>Preço+Tax: {parseFloat(artigo.pricePvp).toFixed(2)}€</Text>
-            <Text style={styles.text}>Descrição: {artigo.description.toString()}</Text>
-          </View>
-          <Image source={{ uri: artigo.image.toString() }} style={styles.artigoImage} />
-        </View>
-      </TouchableOpacity>
-      {artigo.locked !== 1 && (
-      <View style={styles.buttonContainer}>
-        <Button
-          color={'gray'}
-          title={'Remover'}
-          onPress={() => handleRemoverArtigo(artigo.id)}
-        />
-      </View>
-    )}
-    </View>
-  );
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#444444' }}></Text>
+      <Picker
+        style={styles.pickerComponent}
+        selectedValue={filtro}
+        onValueChange={(itemValue) => setFiltro(itemValue)}
+      >
+        <Picker.Item label="Todos" value="Todos" />
+        <Picker.Item label="Usados" value="Usados" />
+        <Picker.Item label="Não Usados" value="Não Usados" />
+      </Picker>
       <FlatList
         data={artigos}
         renderItem={renderItem}
@@ -99,7 +122,7 @@ const styles = StyleSheet.create({
     width: 350,
     backgroundColor: '#fff',
     borderRadius: 10,
-    marginBottom:15,
+    marginBottom: 15,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 3 },
     shadowOpacity: 0.25,
@@ -120,15 +143,15 @@ const styles = StyleSheet.create({
     width: 90,
     height: 90,
     alignSelf: 'center',
-    marginTop: 5, 
+    marginTop: 5,
     marginBottom: 5,
   },
   text: {
     color: '#444444'
   },
   loading: {
-    flex: 1, 
-    justifyContent: 'center', 
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center'
   },
   rowContainer: {
@@ -136,5 +159,28 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     flex: 1,
+  },
+  pickerComponent: {
+    height: 50,
+    width: '100%',
+    color:  'dark',
+    backgroundColor:  'dark' ,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'dark',
+    borderRadius: 7,
+  },
+  buttonModal: {
+    alignItems: 'center',
+    width: 100,
+    padding: 10,
+    borderRadius:10,
+  },
+  modalText: {
+    color: '#ffffff', // Letras brancas
+    fontWeight: 'bold', // Negrito
+    textAlign: 'center', // Centralizado
+    fontSize: 15,
   },
 });
